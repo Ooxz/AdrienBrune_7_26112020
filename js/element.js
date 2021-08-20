d20662f2225ca1f407567f1be4262ba980b6e62d
Ooxz <58948729+Ooxz@users.noreply.github.com>
2021-08-20 08:32:06 +0200

    generateCard update

--- js/element.js
+++ js/element.js
@@ -1,5 +1,22 @@
 // class scontructor to create DOM element with name, type and class name.
 
+// class Element {
+//   constructor (name, type, classname) {
+//     this.name = name
+//     this.type = type
+//     this.classname = classname
+//   }
+
+//   createElt () {
+//     this.name = document.createElement(this.type)
+//     this.name.classlist.add(this.classname)
+//     return this.name
+//   }
+
+//   get elt () {
+//     return this.createElt()
+//   }
+// }
 class Element {
   constructor (name, type, classname) {
     this.name = name
@@ -9,7 +26,7 @@ class Element {
 
   createElt () {
     this.name = document.createElement(this.type)
-    this.name.classlist.add(this.classname)
+    this.name.classList.add(this.classname)
     return this.name
   }
 
--- js/generateCard.js
+++ js/generateCard.js
@@ -1,8 +1,8 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
 import { Element } from './element.js'
 
-// function to generate recipes cards
 function generateCard (param) {
   const section = document.querySelector('.allRecipesCards')
   section.style.display = 'grid'
@@ -17,7 +17,7 @@ function generateCard (param) {
 	  const divBg = new Element('divBg', 'div', 'card__bg').elt
 	  anchor.appendChild(divBg)
 	  const divRecipe = new Element('divRecipe', 'div', 'card__recipe').elt
-	  divBg.appendChild(divRecipe)
+	  anchor.appendChild(divRecipe)
 	  const divTitle = new Element('divTitle', 'div', 'card__title').elt
 	  divRecipe.appendChild(divTitle)
 	  const title = new Element('title', 'h3', 'card__title__h3').elt
@@ -27,7 +27,7 @@ function generateCard (param) {
 	  divTitle.appendChild(divTime)
 	  const iconTime = new Element('iconTime', 'i', 'card__title__clock').elt
 	  divTime.appendChild(iconTime)
-	  iconTime.classlist.add('fa-clock')
+	  iconTime.classList.add('fa-clock')
 	  const time = new Element('time', 'p', 'card__title__time__clock__number').elt
 	  divTime.appendChild(time)
 	  time.textContent = `${param[i].time} min`
@@ -35,8 +35,8 @@ function generateCard (param) {
 	  divRecipe.appendChild(cardContent)
 	  const ulIngredients = new Element('ulIngredients', 'ul', 'card__ingredients').elt
 	  cardContent.appendChild(ulIngredients)
-	  displayIngredients.textContent(param[i].ingredients, ulIngredients)
-	  const description = new Element('description', 'p', 'card__description')
+	  displayIngredients(param[i].ingredients, ulIngredients)
+	  const description = new Element('description', 'p', 'card__description').elt
 	  cardContent.appendChild(description)
 	  description.textContent = `${param[i].description}`
   }
@@ -46,14 +46,15 @@ function displayIngredients (ingredients, ulIngredients) {
   for (const ingredient of ingredients) {
     const liIngredient = new Element('liIngredient', 'li', 'ingredientsList').elt
     ulIngredients.appendChild(liIngredient)
-    const ingredientName = new Element('ingredientName', 'li', 'ingredientsList__item').elt
+    const ingredientName = new Element('ingredientName', 'p', 'ingredientsList__item').elt
     liIngredient.appendChild(ingredientName)
-    ingredientName.textContent = `${ingredient.ingredient}`
-    const quantity = new Element('quantity', 'li', 'ingredientsList__quantity').elt
+    ingredientName.innerHTML = `${ingredient.ingredient}`
+    const quantity = new Element('quantity', 'p', 'ingredientsList__quantity').elt
     liIngredient.appendChild(quantity)
-    if (ingredient.quantity !== undefined) {
+    if (ingredient.quantity != undefined) {
       quantity.innerHTML = ' ' + ':' + ' ' + `${ingredient.quantity}`
     }
   }
 }
+
 export { generateCard }
