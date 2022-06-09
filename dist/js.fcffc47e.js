01c4ae8c434569c66bd65697c30695c63cbbc1e6
Brune <abrune.osteo@gmail.com>
2022-06-09 14:04:50 +0200

    dropdown css update
    pointer event update to avoid clicking on the whole div resulting in all ingredients appearing

--- dist/js.fcffc47e.js
+++ dist/js.fcffc47e.js
@@ -2496,7 +2496,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57976" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59907" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
